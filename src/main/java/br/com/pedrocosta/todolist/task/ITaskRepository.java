781843da4201160bb7973d62bbffb1744a12ae1e
costapedroguilherme <guilhermecostapedro@hotmail.com>
2025-09-24 22:46:21 -0300

    feat: adicionando validação de datas e listagem de tarefas

--- src/main/java/br/com/pedrocosta/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/pedrocosta/todolist/task/ITaskRepository.java
@@ -1,8 +1,11 @@
 package br.com.pedrocosta.todolist.task;
 
+import br.com.pedrocosta.todolist.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
